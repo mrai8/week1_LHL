58df82d67fb9e497255d87f847ae72562d632baa
Mandeep Rai <mandeeprai924@gmail.com>
2017-05-01 18:47:37 -0400

    Initial Commit

---
+++ word_effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  word_effects
+//
+//  Created by Mandeep on 2017-05-01.
+//  Copyright © 2017 Mandeep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
